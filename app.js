02b39efd69a0bb17a105ec366958b9353016618e
Jayant Singh <jayant.singh045@gmail.com>
2021-05-25 18:51:30 +0530

    drum commit

---
+++ app.js
@@ -0,0 +1,39 @@
+const sounds = document.querySelectorAll('.sound')
+const pads = document.querySelectorAll('.pads div')
+const visual = document.querySelector('.visual')
+
+const colors = [
+    '#60d394',
+    '#d36060',
+    '#c060d3',
+    '#ded160',
+    '#6860d3',
+    '#0bbfc5'
+]
+
+
+// Lets get going with the sound here
+pads.forEach((pad, index) => {
+    pad.addEventListener('click', function() {
+        sounds[index].currentTime = 0
+        sounds[index].play()
+        createBubbles(index)
+        
+    })
+})
+
+// Create a function that makes bubbles
+
+const createBubbles = (index) => {
+    const bubble = document.createElement('div')
+    visual.appendChild(bubble)
+    bubble.style.backgroundColor = colors[index]
+    bubble.style.animation = 'jump 1s ease'
+    bubble.addEventListener('animationend', function() {
+        visual.removeChild(this)
+    })
+}
+
+
+
+
